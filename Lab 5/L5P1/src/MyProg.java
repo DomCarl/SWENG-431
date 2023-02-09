d3955699df0687645fb76d9a5ffe66cf76bdafff
DomCarl <dfc5456@psu.edu>
2023-02-09 17:42:28 -0500

    L5P1
    almost done, difficulty writing to output file

--- Lab 5/L5P1/src/MyProg.java
+++ Lab 5/L5P1/src/MyProg.java
@@ -1,4 +1,15 @@
+/**
+ * @author Stuart Walters
+ * @author Robert Myer
+ * @author Dominick Carlucci
+ *
+ * Lab 5 Part 1
+ * SWENG 431
+ * Due Date: 2/12/23 11:59 pm
+ */
+
 import java.io.*;
+import java.util.Random;
 
 public class MyProg
 {
@@ -8,18 +19,57 @@ public class MyProg
 
         BufferedReader br = new BufferedReader(new FileReader(file));
 
-        String str;
-        String[] tokens;
-        String[] equiv = new String[0];
+        String str = "";
+
 
         while ((str = br.readLine()) != null)
         {
-            tokens = str.replace(",", "").split("[;]");
 
-            for (String s : tokens)
+            String[] equiv = str.split("\n");
+            Random rand = new Random();
+
+            FileWriter fw = new FileWriter("src/test.txt");
+
+            for (int i = 0; i < 100; i ++)
             {
-                System.out.println(s.trim());
+                int sum = 0;
+
+                for (int j = 0; j < equiv.length; j++)
+                {
+                    String[] tokens = equiv[j].replace(" ", "").split(";");
+                    int index = rand.nextInt(tokens.length);
+
+                    String[] vals = tokens[index].replace(" ", "").split(",");
+
+                    int lowVal = Integer.parseInt(vals[0]);
+                    int highVal = Integer.parseInt(vals[1]);
+
+                    int val = rand.nextInt((highVal - lowVal) + 1) + lowVal;
+                    sum += check(val, tokens[j]);
+
+                    System.out.println(sum + "\n");
+                }
             }
         }
+    }
+
+    private static int check(int val, String equiv)
+    {
+        String[] tokens = equiv.replace(" ", "").split(";");
+
+        for (int i = 0; i < tokens.length; i++)
+        {
+            String[] vals = tokens[i].replace(" ", "").split(",");
+
+            int lowVal = Integer.parseInt(vals[0]);
+            int highVal = Integer.parseInt(vals[1]);
+
+            if (val >= lowVal && val <= highVal)
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
     }
 }
